4746c4197b0a5ff9be5d0a7fa70724d89f88658e
Ben Mosher <me@benmosher.com>
2025-09-05 18:15:17 -0400

    trying to disable folder suggestions

--- completions.fish
+++ completions.fish
@@ -1,2 +1,6 @@
+complete -c git -f --condition '__fish_git_using_command ensemble'
+
+complete -c git -n '__fish_git_using_command ensemble' -a 'with' -d 'Merge current commit with provided refs'
+
 complete -c git -n '__fish_git_using_command ensemble; and __fish_seen_subcommand_from with' \
     -a '(git for-each-ref --format="%(refname:short)" refs/heads/)' -d 'Branch'
